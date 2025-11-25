51abad3f56ea74abe732d4a1e44e2f4b2eadd9ea
manonbk <145576857+manonbk@users.noreply.github.com>
2025-11-25 17:43:27 +0100

    Update rnnvisu.js

--- rnnvisu.js
+++ rnnvisu.js
@@ -309,3 +309,4 @@
   window.addEventListener('resize', ()=>{ camera.aspect = innerWidth/innerHeight; camera.updateProjectionMatrix(); renderer.setSize(innerWidth, innerHeight); if(points) points.material.uniforms.pointScale.value = window.innerHeight/2; });
 
 
+
